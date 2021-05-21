729a0234994a5f7a138a3a40c646c4148dede7a8
K. Steven <kemering.steven@gmail.com>
2021-05-21 21:57:51 +0200

    finish without good work for effect scale

---
+++ www/script.js
@@ -0,0 +1,26 @@
+//👉 Au clic sur le bouton “Fade Header”, mettez en place un effet en jQuery afin de faire clignoter ce titre 3 fois avant de se réafficher.
+
+$('#button_header').click(function(){
+    $(this).fadeOut().fadeIn().fadeOut().fadeIn().fadeOut().fadeIn()
+})
+
+// 👉 Au clic sur le bouton “Color Stack / off”, mettez en place un effet en jQuery afin de passer les éléments de class stack_container en vert.
+// =>
+// 👉 Le texte du bouton doit être modifié: “Color Stack / off”.
+
+$('#color').click(function(){
+    $('.stack_container').toggleClass("stack_container2");
+    if ($('#color').text() == "Color Stack / OFF"){
+        $('#color').text("Color Stack / ON");
+    }else{
+        $('#color').text("Color Stack / OFF");
+    }
+})
+
+//👉 Au clic sur le bouton + de chaque élément, mettez en place un effet “scale” qui va faire grossir l’élément parent avant de le faire revenir à son état initial.
+// mauvaise fonction car mauvaise taille au retour.
+$('.bigger').click(function(){
+    $(this).parent().effect("scale", {percent: 160}, 1000, function(){
+        $(this).delay(1000).effect("scale", {percent: 65}, 1000)
+    });
+});
